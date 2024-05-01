dcda84af85fc85661288a81ce0744dddfd2d81a4
Michael Friend <michaelf7777@gmail.com>
2024-05-01 17:40:46 -0400

    quick and dirty initial trigger screen

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -44,7 +44,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling:1.4.3")
     implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
     implementation("androidx.compose.foundation:foundation:1.4.3")
-    implementation("androidx.compose.material:material:1.4.3")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.material3:material3:1.2.0-alpha04")
     implementation("androidx.navigation:navigation-compose:2.6.0")
